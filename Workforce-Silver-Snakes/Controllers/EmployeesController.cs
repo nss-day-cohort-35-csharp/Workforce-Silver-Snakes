965b3404998dc805770444c797a2d5f024dd5839
unknown <nwessel429@gmail.com>
2020-01-15 23:23:12 -0600

    added ability to create empployee with computer and department

--- Workforce-Silver-Snakes/Controllers/EmployeesController.cs
+++ Workforce-Silver-Snakes/Controllers/EmployeesController.cs
@@ -5,8 +5,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.Extensions.Configuration;
 using Workforce_Silver_Snakes.Models;
+using Workforce_Silver_Snakes.Models.ViewModels;
 
 namespace Workforce_Silver_Snakes.Controllers
 {
@@ -70,21 +72,55 @@ namespace Workforce_Silver_Snakes.Controllers
         // GET: Employees/Create
         public ActionResult Create()
         {
-            return View();
+            var computers = GetComputers().Select(computers => new SelectListItem
+            {
+                Text = computers.Make + " " + computers.Model,
+                Value = computers.Id.ToString()
+            }).ToList();
+
+            var departments = GetDepartments().Select(departments => new SelectListItem
+            {
+                Text = departments.Name,
+                Value = departments.Id.ToString()
+            }).ToList();
+            var viewModel = new EmployeeCreateViewModel
+            {
+                Employee = new Employee(),
+                Departments = departments,
+                Computers = computers
+            };
+            return View(viewModel);
         }
 
         // POST: Employees/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public ActionResult Create(Employee employee)
         {
             try
             {
-                // TODO: Add insert logic here
+                using (SqlConnection conn = Connection)
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = @"INSERT INTO Employee (FirstName, LastName, ComputerId, Email, DepartmentId, IsSupervisor) VALUES (@firstName, @lastName, @computerId, @departmentId, @email, @IsSupervisor)";
+                        cmd.Parameters.Add(new SqlParameter("@firstName", employee.FirstName));
+                        cmd.Parameters.Add(new SqlParameter("@lastName", employee.LastName));
+                        cmd.Parameters.Add(new SqlParameter("@computerId", employee.ComputerId));
+                        cmd.Parameters.Add(new SqlParameter("@departmentId", employee.Department));
+                        cmd.Parameters.Add(new SqlParameter("@email", employee.Email));
+                        cmd.Parameters.Add(new SqlParameter("@isSupervisor", employee.IsSupervisor));
 
-                return RedirectToAction(nameof(Index));
+
+
+
+                        cmd.ExecuteNonQuery();
+                    }
+                    return RedirectToAction(nameof(Index));
+                }
             }
-            catch
+            catch (Exception ex)
             {
                 return View();
             }
@@ -133,6 +169,63 @@ namespace Workforce_Silver_Snakes.Controllers
             catch
             {
                 return View();
+            }
+        }
+        private List<Computer> GetComputers()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT Id, Make, Model FROM Computer";
+
+                    var reader = cmd.ExecuteReader();
+
+                    var computers = new List<Computer>();
+
+                    while (reader.Read())
+                    {
+                        computers.Add(new Computer
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Make = reader.GetString(reader.GetOrdinal("Make")),
+                            Model = reader.GetString(reader.GetOrdinal("Model"))
+                        });
+                    }
+
+                    reader.Close();
+
+                    return computers;
+                }
+            }
+        }
+        private List<Department> GetDepartments()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT Id, Name FROM Department";
+
+                    var reader = cmd.ExecuteReader();
+
+                    var departments = new List<Department>();
+
+                    while (reader.Read())
+                    {
+                        departments.Add(new Department
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Name = reader.GetString(reader.GetOrdinal("Name"))
+                        });
+                    }
+
+                    reader.Close();
+
+                    return departments;
+                }
             }
         }
     }
